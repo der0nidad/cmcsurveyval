2ee13041405f327b49359005fbb8ce0ebf2d7a0c
Yaroslav Salimov <i.salimov@corp.mail.ru>
2020-02-11 19:50:14 +0300

    Initial commit and some experiments

---
+++ frontend/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
