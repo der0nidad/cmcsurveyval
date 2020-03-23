9f5e5785a0f65c0e04b42c701a047b17e00c75cf
Y S <der0nidad@gmail.com>
2020-03-23 12:55:15 +0300

    added better drawer

--- frontend/src/components/RouterComponent/index.jsx
+++ frontend/src/components/RouterComponent/index.jsx
@@ -3,25 +3,43 @@ import PropTypes from 'prop-types';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Auth from '../Auth';
-import SignUp from '../SignUp';
-import { LeadList } from '../LeadList';
+import { Drawer } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import List from '@material-ui/core/List';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Divider from '@material-ui/core/Divider';
+import MailIcon from '@material-ui/icons/Mail';
+
+import { closeLeftMenu, openLeftMenu } from '../../store/actions/flags.actions';
 import { whoAmIAction } from '../../store/actions/routerComponent.actions';
+import { LeadList } from '../LeadList';
+import SignUp from '../SignUp';
+import Auth from '../Auth';
 
 function Home() {
   return <h2>Home2</h2>;
 }
+
 class RouterComp extends React.Component {
   static propTypes = {
     user: PropTypes.shape({
       username: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired,
     }),
+    menuOpened: PropTypes.bool,
     checkUserIsAuthenticated: PropTypes.func.isRequired,
+    openMenu: PropTypes.func.isRequired,
+    closeMenu: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
     user: null,
+    menuOpened: false,
   };
 
   componentDidMount() {
@@ -29,10 +47,53 @@ class RouterComp extends React.Component {
     checkUserIsAuthenticated();
   }
 
+  handleOpenMenu = () => {
+    const { openMenu } = this.props;
+    openMenu();
+  };
 
   render() {
+    const { menuOpened, closeMenu } = this.props;
+    const list = (anchor) => (
+      <div
+        // className={clsx(classes.list, {
+        //   [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+        // })}
+        role="presentation"
+        onClick={closeMenu}
+        onKeyDown={closeMenu}
+      >
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
     return (
       <Router>
+        <Button
+          onClick={this.handleOpenMenu}
+        >
+          Open Menu
+        </Button>
+        <Drawer
+          open={menuOpened}
+        >
+          {list('left')}
+        </Drawer>
         <div>
           {/*  <nav> */}
           {/*    <ul> */}
@@ -84,9 +145,12 @@ class RouterComp extends React.Component {
 
 const mapStateToProps = (state) => ({
   user: state.auth.user,
+  menuOpened: state.flags.menuOpened,
 });
 const mapDispatchToProps = (dispatch) => ({
   checkUserIsAuthenticated: () => dispatch(whoAmIAction()),
+  openMenu: () => dispatch(openLeftMenu()),
+  closeMenu: () => dispatch(closeLeftMenu()),
   handleDecrementClick: () => dispatch({ type: 'DECREMENT' }),
 });
 
---
+++ frontend/src/store/actions/flags.actions.js
@@ -0,0 +1,8 @@
+import {CLOSE_LEFT_MENU, OPEN_LEFT_MENU} from '../actionTypes';
+
+export const openLeftMenu = () => ({
+  type: OPEN_LEFT_MENU,
+});
+export const closeLeftMenu = () => ({
+  type: CLOSE_LEFT_MENU,
+});
